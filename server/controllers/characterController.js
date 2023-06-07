4b3ee41be2b8ec09b0fd9004f437fddb5c2244b4
arcioni julien <julien.arcioni@gmail.com>
2023-06-07 11:05:00 +0200

    Ajout description à la création d'un personnage

--- server/controllers/characterController.js
+++ server/controllers/characterController.js
@@ -13,21 +13,63 @@ const openai = new OpenAIApi(configuration);
 // const openaiClient = new openai.OpenAIApi(apiKey);
 const { authenticateToken } = require('../middleware/auth.middleware');
 
-
 // Créer un personnage dans un univers spécifié
-router.post('/game/:id', authenticateToken, (req, res) => {
-    const {id} = req.params;
-    const {nom} = req.body;
-    const query = 'INSERT INTO characters (name, game_id) VALUES (?, ?)';
-    connection.query(query, [nom, id], (error, results) => {
-        if (error) {
-            console.error('Erreur lors de la création du personnage :', error);
-            res.status(500).json({error: 'Erreur lors de la création du personnage'});
+router.post('/game/:id', authenticateToken, async (req, res) => {
+    const { id } = req.params;
+    const { nom } = req.body;
+
+    // Vérifier si le personnage existe déjà dans la base de données
+    const checkQuery = 'SELECT * FROM characters WHERE name = ?';
+    connection.query(checkQuery, [nom], async (checkError, checkResults) => {
+        if (checkError) {
+            console.error('Erreur lors de la vérification de l\'existence du personnage :', checkError);
+            res.status(500).json({ error: 'Erreur lors de la vérification de l\'existence du personnage' });
+        } else if (checkResults.length > 0) {
+            res.status(409).json({ error: 'Le personnage existe déjà' });
         } else {
-            res.status(201).json({message: 'Personnage créé avec succès'});
+            const query = 'INSERT INTO characters (name, game_id) VALUES (?, ?)';
+            connection.query(query, [nom, id], async (error, results) => {
+                if (error) {
+                    console.error('Erreur lors de la création du personnage :', error);
+                    res.status(500).json({ error: 'Erreur lors de la création du personnage' });
+                } else {
+                    const characterId = results.insertId;
+
+                    try {
+                        const gameQuery = 'SELECT nom FROM games WHERE id = ?';
+                        connection.query(gameQuery, [id], async (gameError, gameResults) => {
+                            if (gameError) {
+                                console.error('Erreur lors de la récupération du jeu associé au personnage :', gameError);
+                                res.status(500).json({ error: 'Erreur lors de la récupération du jeu associé au personnage' });
+                            } else if (gameResults.length === 0) {
+                                res.status(404).json({ error: 'Jeu associé au personnage non trouvé' });
+                            } else {
+                                const gameName = gameResults[0].nom;
+
+                                const description = await generateCharacterDescription(nom, gameName);
+
+                                const updateQuery = 'UPDATE characters SET description = ? WHERE id = ?';
+                                connection.query(updateQuery, [description, characterId], (updateError) => {
+                                    if (updateError) {
+                                        console.error('Erreur lors de la mise à jour de la description du personnage :', updateError);
+                                        res.status(500).json({ error: 'Erreur lors de la mise à jour de la description du personnage' });
+                                    } else {
+                                        res.status(201).json({ message: 'Personnage créé avec succès' });
+                                    }
+                                });
+                            }
+                        });
+                    } catch (generationError) {
+                        console.error('Erreur lors de la génération de la description du personnage :', generationError);
+                        res.status(500).json({ error: 'Erreur lors de la génération de la description du personnage' });
+                    }
+                }
+            });
         }
     });
 });
+
+
 
 // Récupérer un personnage par son ID
 router.get('/:id', authenticateToken, (req, res) => {
@@ -106,14 +148,15 @@ async function generateCharacterDescription(characterName, gameName) {
         model: "text-davinci-003",
         prompt,
         temperature: 1,
-        max_tokens: 100,
+        max_tokens: 50,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
         stop: ["AI:", "Human:"],
     });
 
-    const description = response.data.choices[0].text;
+    const description1 = response.data.choices[0].text;
+    const description = description1.replace(/^\s*\n/g, '');
     return description;
 }
 
